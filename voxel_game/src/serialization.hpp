f57e5185a0098da6360c30bd6e6fbe244810e0bb
Anon Ymous <rbnelr@gmx.de>
2020-10-24 19:37:09 +0200

    WFC pattern extraction

--- voxel_game/src/serialization.hpp
+++ voxel_game/src/serialization.hpp
@@ -67,20 +67,31 @@ T load (char const* filename) {
 }
 
 #define _JSON_EXPAND( x ) x
-#define _JSON_GET_MACRO(_1,_2,_3,_4,_5,_6, _7, _8, _9, _10, _11, NAME,...) NAME
+#define _JSON_GET_MACRO(_1,_2,_3,_4,_5,_6, _7, _8, _9, _10, _11, _12, _13, _14, _15, _16, _17, _18, _19, _20, NAME,...) NAME
 
-#define _JSON_PASTE(...) _JSON_EXPAND(_JSON_GET_MACRO(__VA_ARGS__, _JSON_PASTE11, _JSON_PASTE10, _JSON_PASTE9, _JSON_PASTE8, _JSON_PASTE7, \
-                                                                   _JSON_PASTE6, _JSON_PASTE5, _JSON_PASTE4, _JSON_PASTE3, _JSON_PASTE2, _JSON_PASTE1)(__VA_ARGS__))
-#define _JSON_PASTE2(func,  v1)                                      func(v1)
-#define _JSON_PASTE3(func,  v1, v2)                                  _JSON_PASTE2(func, v1) _JSON_PASTE2(func, v2)
-#define _JSON_PASTE4(func,  v1, v2, v3)                              _JSON_PASTE2(func, v1) _JSON_PASTE3(func, v2, v3)
-#define _JSON_PASTE5(func,  v1, v2, v3, v4)                          _JSON_PASTE2(func, v1) _JSON_PASTE4(func, v2, v3, v4)
-#define _JSON_PASTE6(func,  v1, v2, v3, v4, v5)                      _JSON_PASTE2(func, v1) _JSON_PASTE5(func, v2, v3, v4, v5)
-#define _JSON_PASTE7(func,  v1, v2, v3, v4, v5, v6)                  _JSON_PASTE2(func, v1) _JSON_PASTE6(func, v2, v3, v4, v5, v6)
-#define _JSON_PASTE8(func,  v1, v2, v3, v4, v5, v6, v7)              _JSON_PASTE2(func, v1) _JSON_PASTE7(func, v2, v3, v4, v5, v6, v7)
-#define _JSON_PASTE9(func,  v1, v2, v3, v4, v5, v6, v7, v8)          _JSON_PASTE2(func, v1) _JSON_PASTE8(func, v2, v3, v4, v5, v6, v7, v8)
-#define _JSON_PASTE10(func, v1, v2, v3, v4, v5, v6, v7, v8, v9)      _JSON_PASTE2(func, v1) _JSON_PASTE8(func, v2, v3, v4, v5, v6, v7, v8, v9)
-#define _JSON_PASTE11(func, v1, v2, v3, v4, v5, v6, v7, v8, v9, v10) _JSON_PASTE2(func, v1) _JSON_PASTE8(func, v2, v3, v4, v5, v6, v7, v8, v9, v10)
+#define _JSON_PASTE(...) _JSON_EXPAND(_JSON_GET_MACRO(__VA_ARGS__, \
+	_JSON_PASTE20, _JSON_PASTE19, _JSON_PASTE18, _JSON_PASTE17, _JSON_PASTE16, _JSON_PASTE15, _JSON_PASTE14, _JSON_PASTE13, _JSON_PASTE12, _JSON_PASTE11, \
+	_JSON_PASTE10, _JSON_PASTE9, _JSON_PASTE8, _JSON_PASTE7, _JSON_PASTE6, _JSON_PASTE5, _JSON_PASTE4, _JSON_PASTE3, _JSON_PASTE2, _JSON_PASTE1 \
+	)(__VA_ARGS__))
+#define _JSON_PASTE2(func,  v1)																						func(v1)
+#define _JSON_PASTE3(func,  v1, v2)																					_JSON_PASTE2(func, v1) _JSON_PASTE2(func, v2)
+#define _JSON_PASTE4(func,  v1, v2, v3)																				_JSON_PASTE2(func, v1) _JSON_PASTE3(func, v2, v3)
+#define _JSON_PASTE5(func,  v1, v2, v3, v4)																			_JSON_PASTE2(func, v1) _JSON_PASTE4(func, v2, v3, v4)
+#define _JSON_PASTE6(func,  v1, v2, v3, v4, v5)																		_JSON_PASTE2(func, v1) _JSON_PASTE5(func, v2, v3, v4, v5)
+#define _JSON_PASTE7(func,  v1, v2, v3, v4, v5, v6)																	_JSON_PASTE2(func, v1) _JSON_PASTE6(func, v2, v3, v4, v5, v6)
+#define _JSON_PASTE8(func,  v1, v2, v3, v4, v5, v6, v7)																_JSON_PASTE2(func, v1) _JSON_PASTE7(func, v2, v3, v4, v5, v6, v7)
+#define _JSON_PASTE9(func,  v1, v2, v3, v4, v5, v6, v7, v8)															_JSON_PASTE2(func, v1) _JSON_PASTE8(func, v2, v3, v4, v5, v6, v7, v8)
+#define _JSON_PASTE10(func, v1, v2, v3, v4, v5, v6, v7, v8, v9)														_JSON_PASTE2(func, v1) _JSON_PASTE9(func, v2, v3, v4, v5, v6, v7, v8, v9)
+#define _JSON_PASTE11(func, v1, v2, v3, v4, v5, v6, v7, v8, v9, v10)												_JSON_PASTE2(func, v1) _JSON_PASTE10(func, v2, v3, v4, v5, v6, v7, v8, v9, v10)
+#define _JSON_PASTE12(func, v1, v2, v3, v4, v5, v6, v7, v8, v9, v10, v11)											_JSON_PASTE2(func, v1) _JSON_PASTE11(func, v2, v3, v4, v5, v6, v7, v8, v9, v10, v11)
+#define _JSON_PASTE13(func, v1, v2, v3, v4, v5, v6, v7, v8, v9, v10, v11, v12)										_JSON_PASTE2(func, v1) _JSON_PASTE12(func, v2, v3, v4, v5, v6, v7, v8, v9, v10, v11, v12)
+#define _JSON_PASTE14(func, v1, v2, v3, v4, v5, v6, v7, v8, v9, v10, v11, v12, v13)									_JSON_PASTE2(func, v1) _JSON_PASTE13(func, v2, v3, v4, v5, v6, v7, v8, v9, v10, v11, v12, v13)
+#define _JSON_PASTE15(func, v1, v2, v3, v4, v5, v6, v7, v8, v9, v10, v11, v12, v13, v14)							_JSON_PASTE2(func, v1) _JSON_PASTE14(func, v2, v3, v4, v5, v6, v7, v8, v9, v10, v11, v12, v13, v14)
+#define _JSON_PASTE16(func, v1, v2, v3, v4, v5, v6, v7, v8, v9, v10, v11, v12, v13, v14, v15)						_JSON_PASTE2(func, v1) _JSON_PASTE15(func, v2, v3, v4, v5, v6, v7, v8, v9, v10, v11, v12, v13, v14, v15)
+#define _JSON_PASTE17(func, v1, v2, v3, v4, v5, v6, v7, v8, v9, v10, v11, v12, v13, v14, v15, v16)					_JSON_PASTE2(func, v1) _JSON_PASTE16(func, v2, v3, v4, v5, v6, v7, v8, v9, v10, v11, v12, v13, v14, v15, v16)
+#define _JSON_PASTE18(func, v1, v2, v3, v4, v5, v6, v7, v8, v9, v10, v11, v12, v13, v14, v15, v16, v17)				_JSON_PASTE2(func, v1) _JSON_PASTE17(func, v2, v3, v4, v5, v6, v7, v8, v9, v10, v11, v12, v13, v14, v15, v16, v17)
+#define _JSON_PASTE19(func, v1, v2, v3, v4, v5, v6, v7, v8, v9, v10, v11, v12, v13, v14, v15, v16, v17, v18)		_JSON_PASTE2(func, v1) _JSON_PASTE18(func, v2, v3, v4, v5, v6, v7, v8, v9, v10, v11, v12, v13, v14, v15, v16, v17, v18)
+#define _JSON_PASTE20(func, v1, v2, v3, v4, v5, v6, v7, v8, v9, v10, v11, v12, v13, v14, v15, v16, v17, v18, v19)	_JSON_PASTE2(func, v1) _JSON_PASTE19(func, v2, v3, v4, v5, v6, v7, v8, v9, v10, v11, v12, v13, v14, v15, v16, v17, v18, v19)
 
 #define _JSON_TO(v1) j[#v1] = t.v1;
 #define _JSON_FROM(v1) if (j.contains(#v1)) j.at(#v1).get_to(t.v1); // try get value from json
@@ -94,6 +105,42 @@ T load (char const* filename) {
     void from_json(const nlohmann::ordered_json& j, Type& t) { _JSON_EXPAND(_JSON_PASTE(_JSON_FROM, __VA_ARGS__)) }
 
 namespace nlohmann {
+	template <>	struct adl_serializer<int2> {
+		using type = int2;
+		static void to_json(ordered_json& j, const type& val) {
+			j = { val.x, val.y };
+		}
+		static void from_json(const ordered_json& j, type& val) {
+			j.at(0).get_to(val.x);
+			j.at(1).get_to(val.y);
+		}
+	};
+
+	template <>	struct adl_serializer<int3> {
+		using type = int3;
+		static void to_json(ordered_json& j, const type& val) {
+			j = { val.x, val.y };
+		}
+		static void from_json(const ordered_json& j, type& val) {
+			j.at(0).get_to(val.x);
+			j.at(1).get_to(val.y);
+			j.at(2).get_to(val.z);
+		}
+	};
+
+	template <>	struct adl_serializer<int4> {
+		using type = int4;
+		static void to_json(ordered_json& j, const type& val) {
+			j = { val.x, val.y };
+		}
+		static void from_json(const ordered_json& j, type& val) {
+			j.at(0).get_to(val.x);
+			j.at(1).get_to(val.y);
+			j.at(2).get_to(val.z);
+			j.at(3).get_to(val.w);
+		}
+	};
+
 	template <>	struct adl_serializer<float2> {
 		using type = float2;
 		static void to_json(ordered_json& j, const type& val) {
--- voxel_game/src/tilemap_test.hpp
+++ voxel_game/src/tilemap_test.hpp
@@ -3,17 +3,57 @@
 #include "serialization.hpp"
 #include <algorithm>
 
+template <typename T>
+struct Array2D {
+	MOVE_ONLY_CLASS(Array2D)
+
+	int w=0, h=0;
+	T* data = nullptr;
+
+	Array2D () {}
+	Array2D (int w, int h): w{w}, h{h} {
+		data = (T*)malloc(w * h * sizeof(T));
+	}
+	~Array2D () {
+		if (data)
+			free(data);
+	}
+
+	T const& get (int x, int y) const {
+		return data[y * w + x];
+	}
+	T& get (int x, int y) {
+		return data[y * w + x];
+	}
+
+	T* get_row (int y) {
+		return data + y * w;
+	}
+
+	void set (int x, int y, T& val) {
+		data[y * w + x] = std::move(val);
+	}
+	void set (int x, int y, T&& val) {
+		data[y * w + x] = val;
+	}
+};
+template <typename T>
+inline void swap (Array2D<T>& l, Array2D<T>& r) {
+	std::swap(l.w, r.w);
+	std::swap(l.h, r.h);
+	std::swap(l.data, r.data);
+}
+
 struct TilemapTest {
-	SERIALIZE(TilemapTest, tilemap_filename, pattern_N, patterns_inc_border)
+	SERIALIZE(TilemapTest, enable, input_filename)
 	
 	bool enable = 1;
 
-	std::string tilemap_filename;
-
-	Image<srgb8> tilemap_raw; // color image
-	Image<uint8_t> tilemap; // paletized color image where palette is tiles
-
 	static constexpr uint8_t NULLID = (uint8_t)-1;
+
+	static constexpr int2 dirs[4] = {
+		int2(-1, 0), int2(+1, 0), int2(0, -1), int2(0, +1),
+	};
 
 	struct Tile {
 		SERIALIZE(Tile, name, color)
@@ -22,119 +62,265 @@ struct TilemapTest {
 		srgb8		color;
 		int			count = 0;
 	};
-	std::vector<Tile> tiles;
+	struct WFCInput {
+		SERIALIZE(WFCInput, sample_filename, size, N, symmetrize, periodic_x, periodic_y, border_x, border_y, tiles)
+
+		std::string sample_filename;
+
+		int N = 2;
+
+		bool symmetrize = false;
+		bool periodic_x = false, periodic_y = false;
+		bool border_x = false, border_y = false;
+		
+		Image<srgb8> sample_raw; // color image
+		Image<uint8_t> sample; // palettized color image where palette is tiles
+
+		std::vector<Tile> tiles;
+
+		int2 size = 64; // output size
+	};
+
+	struct Pattern {
+		// number of instances of this pattern in the source
+		int count;
+
+		// number of patterns that are compatible when at an offset in the four directions
+		int compatible_count[4];
+
+		std::vector<int> _compatible_patterns[4]; // not needed in algo, only for debugging visualization
+	};
+
+	std::string input_filename;
+	WFCInput I;
+
+	int N; // copy of N to make displaying of patterns safe
+	
+	int pattern_total; // number of all instances of patterns in the source
+
+	std::vector<Pattern> patterns;
+	std::vector<uint8_t> patterns_data;
+	
+	////
+	std::vector<int> symmetry_modes[8]; // identity, rot cw 90, rot cw 180, rot ccw 90, mirror horiz, 4x mirror horiz + rotations 
+	void create_symmetry_modes () {
+		for (auto& v : symmetry_modes)	v.resize(N*N);
+
+		// copy indices in src into dst, rotated by 90 deg cw
+		auto rotate_cw = [&] (std::vector<int>& src, std::vector<int>& dst) {
+			for (int y=0; y<N; ++y)
+				for (int x=0; x<N; ++x)
+					dst[(N-1-x) * N + y] = src[y * N + x];
+		};
+		auto flip_x = [&] (std::vector<int>& src, std::vector<int>& dst) {
+			for (int y=0; y<N; ++y)
+				for (int x=0; x<N; ++x)
+					dst[y * N + (N-1-x)] = src[y * N + x];
+		};
+
+		for (int i=0; i<N*N; ++i) symmetry_modes[0][i] = i;
+
+		for (int i=1; i<4; ++i)
+			rotate_cw(symmetry_modes[i-1], symmetry_modes[i]);
+
+		flip_x(symmetry_modes[0], symmetry_modes[4]);
+
+		for (int i=5; i<8; ++i)
+			rotate_cw(symmetry_modes[i-1], symmetry_modes[i]);
+	}
 
 	void gather_tiles () {
-		load(prints("%s.tiles.json", tilemap_filename.c_str()).c_str(), &tiles);
+		for (auto& t : I.tiles) {
+			t.count = 0;
+		}
 
-		tilemap = Image<uint8_t>(tilemap_raw.size);
+		I.sample = Image<uint8_t>(I.sample_raw.size);
 
-		for (int y=0; y<tilemap_raw.size.y; ++y) {
-			for (int x=0; x<tilemap_raw.size.x; ++x) {
-				auto col = tilemap_raw.get(x,y);
+		for (int y=0; y<I.sample.size.y; ++y) {
+			for (int x=0; x<I.sample.size.x; ++x) {
+				auto col = I.sample_raw.get(x,y);
 
-				int i = indexof(tiles, col, [] (Tile& l, srgb8 r) { return l.color == r; });
+				int i = indexof(I.tiles, col, [] (Tile& l, srgb8 r) { return l.color == r; });
 				if (i < 0) { // new tile, create tile in list
-					assert(tiles.size() < 254);
-					tiles.push_back({ "", col, 1 });
+					assert(I.tiles.size() < 254);
+					I.tiles.push_back({ "", col, 1 });
 				} else { // existing tile, count occurance
-					tiles[i].count++;
+					I.tiles[i].count++;
 				}
 
-				tilemap.set(x,y, (uint8_t)i);
+				I.sample.set(x,y, (uint8_t)i);
 			}
 		}
 	}
 
-	int pattern_N = 3;
-	bool patterns_inc_border = false;
-
-	struct Pattern {
-		int count = 0;
-	};
-	std::vector<Pattern> patterns;
-	std::vector<uint8_t> patterns_data;
-
 	void gather_patterns () {
+		N = I.N;
+
+		create_symmetry_modes();
+
 		patterns.clear();
 		patterns_data.clear();
 
-		// either include all NxN patterns that are completely on the image or
-		//  all NxN patterns that touch the image with at least one pixel overlap
-		int start = patterns_inc_border ? 1 - pattern_N				: 0;
-		int2 end  = patterns_inc_border ? tilemap.size : tilemap.size - pattern_N;
+		pattern_total = 0;
 
-		for (int y=start; y<end.y; ++y) {
-			for (int x=start; x<end.x; ++x) {
+		// sample lower border if border is activated (don't wrap to the left becaue then we would sample more often on the border, keep sampling uniform)
+		int startx = I.border_x ? 1 - N : 0;
+		int starty = I.border_y ? 1 - N : 0;
+		// sample beyond image either the border or by wrapping around
+		int endx   = I.border_x || I.periodic_x ? I.sample.size.x : I.sample.size.x - N;
+		int endy   = I.border_y || I.periodic_y ? I.sample.size.y : I.sample.size.y - N;
 
-				// alloc pattern
-				size_t offs = patterns_data.size();
-				patterns_data.resize( offs + pattern_N*pattern_N );
-				uint8_t* data = &patterns_data[offs];
+		std::vector<uint8_t> tmp;
+		tmp.resize(N*N * ARRLEN(symmetry_modes));
 
-				// extract pattern from tilemap
-				for (int py=0; py<pattern_N; ++py) {
-					for (int px=0; px<pattern_N; ++px) {
+		for (int y=starty; y<endy; ++y) {
+			for (int x=startx; x<endx; ++x) {
+
+				// extract pattern from sample image into tmp[0]
+				for (int py=0; py<N; ++py) {
+					for (int px=0; px<N; ++px) {
+						int sx = x + px;
+						int sy = y + py;
+
+						if (I.periodic_x) sx = wrap(sx, I.sample.size.x);
+						if (I.periodic_y) sy = wrap(sy, I.sample.size.y);
+
 						uint8_t id = NULLID;
-						if (	x+px >= 0 && x+px < tilemap.size.x &&
-								y+py >= 0 && y+py < tilemap.size.y ) {
-							id = tilemap.get(x+px, y+py);
+						if (	sx >= 0 && sx < I.sample.size.x &&
+								sy >= 0 && sy < I.sample.size.y ) {
+							id = I.sample.get(sx,sy);
 						}
-						data[py * pattern_N + px] = id;
+
+						tmp[py * N + px] = id;
 					}
 				}
 
-				// scan patterns to see if pattern is unique
-				bool unique = true;
-				for (int i=0; i<(int)patterns.size(); ++i) {
-					int cmp = memcmp(data, &patterns_data[i * pattern_N*pattern_N], pattern_N*pattern_N * sizeof(uint8_t));
-					if (cmp == 0) {
-						unique = false;
-						patterns[i].count++; // count pattern
-						break;
+				for (int s=0; s<(I.symmetrize ? ARRLEN(symmetry_modes) : 1); ++s) {
+					uint8_t* pat = &tmp[s * N*N];
+
+					// create symmetry mode of pattern
+					for (int i=0; i<N*N; ++i) {
+						pat[ symmetry_modes[s][i] ] = tmp[i];
+					}
+
+					// scan patterns to see if pattern is unique
+					bool unique = true;
+					for (int i=0; i<(int)patterns.size(); ++i) {
+						int cmp = memcmp(pat, &patterns_data[i * N*N], N*N * sizeof(uint8_t));
+						if (cmp == 0) {
+							unique = false;
+							patterns[i].count++; // count pattern
+							pattern_total++;
+							break;
+						}
+					}
+
+					if (unique) {
+						// register pattern
+						patterns.push_back({ 1 });
+
+						// alloc pattern_data
+						size_t offs = patterns_data.size();
+						patterns_data.resize( offs + N*N );
+						uint8_t* data = &patterns_data[offs];
+
+						memcpy(data, pat, N*N * sizeof(uint8_t));
 					}
 				}
+			}
+		}
 
-				if (unique) {
-					// register pattern
-					patterns.push_back({ 1 });
-				} else {
-					// delete duplicate pattern data
-					patterns_data.resize(offs);
+		for (int d=0; d<4; ++d) {
+			for (int a=0; a<(int)patterns.size(); ++a) {
+				
+				patterns[a].compatible_count[d] = 0;
+				
+				for (int b=0; b<(int)patterns.size(); ++b) {
+					if (pattern_compatible(a, b, dirs[d])) {
+						patterns[a].compatible_count[d]++;
+						patterns[a]._compatible_patterns[d].push_back(b);
+					}
 				}
 			}
 		}
 	}
+	// Can Pattern B be placed at offs relative to pattern B so that the overlapping region has identical tiles in both?
+	bool pattern_compatible (int a, int b, int2 offs) {
+		// check overlapping rect of pattern A agianst pattern B
+		int x0 =     max(offs.x, 0);
+		int y0 =     max(offs.y, 0);
+		int x1 = N + min(offs.x, 0);
+		int y1 = N + min(offs.y, 0);
 
-	void load_tilemap () {
-		if (!Image<srgb8>::load_from_file(tilemap_filename.c_str(), &tilemap_raw)) {
-			clog("[TilemapTest] Could not load tilemap from file!");
-			return;
+		for (int ya=y0; ya<y1; ++ya) {
+			for (int xa=x0; xa<x1; ++xa) {
+				int xb = xa - offs.x;
+				int yb = ya - offs.y;
+				assert(xa >= 0 && xa < N && ya >= 0 && ya < N);
+				assert(xb >= 0 && xb < N && yb >= 0 && yb < N);
+
+				uint8_t pxa = patterns_data[a * N*N + ya * N + xa];
+				uint8_t pxb = patterns_data[b * N*N + yb * N + xb];
+				if (pxa != pxb)
+					return false;
+			}
+		}
+		return true;
+	}
+
+	void compile () {
+		if (!Image<srgb8>::load_from_file(prints("WFC/%s", I.sample_filename.c_str()).c_str(), &I.sample_raw)) {
+			clog(WARNING, "[TilemapTest] Could not load tilemap from file!");
+			I.sample_raw = Image<srgb8>();
 		}
 
 		gather_tiles();
 		gather_patterns();
+
+		show_pattern_details = false;
 	}
 
-	bool regen = 1;
+	struct Cell {
+		
+	};
 
-	static constexpr int SIZE = 64;
-	int world[SIZE][SIZE]; // tile ids
+	std::vector<Cell> output;
+
+	void generate () {
+		output.resize(I.size.x * I.size.y);
+
+	}
 
 	bool init = true;
+	bool reload = true; // reload input config from json
+	bool recompile = true; // regenerate tile lists and patterns
+	bool regenerate = true; // regenerate new output
+
 	void update () {
 		if (!enable) return;
 
 		if (init) {
 			load("tilemap_test.json", this);
-			load_tilemap();
 			init = false;
 		}
 
-		if (regen) {
-			memset(world, -1, sizeof(world));
+		if (reload) {
+			if (!load(prints("WFC/%s.json", input_filename.c_str()).c_str(), &I)) {
+				I = WFCInput();
+				I.sample_filename = input_filename + ".png";
+			}
+			reload = false;
+			recompile = true;
+		}
 
-			regen = false;
+		if (recompile) {
+			compile();
+			recompile = false;
+		}
+
+		if (regenerate) {
+			generate();
+			regenerate = false;
 		}
 
 		//for (int y=0; y<SIZE; ++y) {
@@ -166,6 +352,12 @@ struct TilemapTest {
 	}
 	
 	bool show_patterns = false;
+	
+	bool show_pattern_details;
+	int selected_pattern = -1;
+
+	static constexpr int IMGUI_TILE_SZ = 28;
+
 	void imgui () {
 		ImGui::Checkbox("TilemapTest enable", &enable);
 
@@ -174,91 +366,150 @@ struct TilemapTest {
 			ImGui::SameLine();
 			if (ImGui::Button("Load Settings")) load("tilemap_test.json", this);
 
-			ImGui::InputText("tilemap_filename", &tilemap_filename);
-			if (ImGui::Button("Load")) load_tilemap();
+			ImGui::InputText("input_filename", &input_filename);
+
+			reload = ImGui::Button("Load") || reload;
 			ImGui::SameLine();
-			if (ImGui::Button("Save Tile Config")) save(prints("%s.tiles.json", tilemap_filename.c_str()).c_str(), tiles);
+			if (ImGui::Button("Save")) save(prints("WFC/%s.json", input_filename.c_str()).c_str(), I);
 
-			ImGui::Text("Tilemap:");
-			ImGui::Text("Size: %d x %d px", tilemap.size.x, tilemap.size.y);
+			if (ImGui::TreeNodeEx("Input", ImGuiTreeNodeFlags_DefaultOpen)) {
 
-			ImGui::SliderInt("pattern_N", &pattern_N, 2, 8);
-			ImGui::Checkbox("patterns_inc_border", &patterns_inc_border);
+				ImGui::InputText("sample_filename", &I.sample_filename);
+				
+				ImGui::Text("Sample:");
+				ImGui::Text("Size: %d x %d px", I.sample.size.x, I.sample.size.y);
 
-			regen = ImGui::Button("Regen");
+				recompile = ImGui::SliderInt("pattern_N", &I.N, 2, 8) || recompile;
+				if (recompile) {
+					int a = 5;
+				}
 
-			static constexpr int IMGUI_TILE_SZ = 28;
+				recompile = ImGui::Checkbox("symmetrize", &I.symmetrize) || recompile;
+
+				recompile = ImGui::Checkbox("border X", &I.border_x) || recompile;
+				ImGui::SameLine();
+				recompile = ImGui::Checkbox("border Y", &I.border_y) || recompile;
+
+				recompile = ImGui::Checkbox("Periodic X", &I.periodic_x) || recompile;
+				ImGui::SameLine();
+				recompile = ImGui::Checkbox("Periodic Y", &I.periodic_y) || recompile;
+
+				ImGui::TreePop();
+			}
+
+			recompile = ImGui::Button("Recomp") || recompile;
+			regenerate = ImGui::Button("Regen") || regenerate;
 
 			if (	ImGui::TreeNodeEx("Tiles", ImGuiTreeNodeFlags_NoTreePushOnOpen) &&
 					ImGui::BeginTable("tiles", 3, ImGuiTableFlags_Borders|ImGuiTableFlags_Resizable)) {
 				ImGui::TableSetupColumn("Tile Color", ImGuiTableColumnFlags_WidthFixed, IMGUI_TILE_SZ);
 				ImGui::TableSetupColumn("Tile Name", ImGuiTableColumnFlags_WidthFixed, 80);
 				ImGui::TableSetupColumn("Count");
-				for (int id=0; id<(int)tiles.size(); ++id) {
+				for (int id=0; id<(int)I.tiles.size(); ++id) {
 					ImGui::PushID(id);
 					ImGui::TableNextCell();
-					imgui_ColorEdit("###color", &tiles[id].color);
+					imgui_ColorEdit("###color", &I.tiles[id].color);
 					ImGui::TableNextCell();
-					ImGui::InputText("###name", &tiles[id].name);
+					ImGui::SetNextItemWidth(-1);
+					ImGui::InputText("###name", &I.tiles[id].name);
 					ImGui::TableNextCell();
-					ImGui::Text("%5d", tiles[id].count);
+					ImGui::Text("%5d", I.tiles[id].count);
 					ImGui::PopID();
 				}
 				ImGui::EndTable();
 			}
 
 			ImGui::Checkbox("Show Patterns", &show_patterns);
-			if (show_patterns) {
-				ImGui::Begin("Patterns", &show_patterns);
-
-				auto wndsz = ImGui::GetContentRegionAvail();
-
-				float cw = IMGUI_TILE_SZ*pattern_N + ImGui::GetStyle().CellPadding.x*2;
-				int w = max(1, floori(wndsz.x / cw));
-
-				if (ImGui::BeginTable("patterns", w, ImGuiTableFlags_Borders|ImGuiTableFlags_SizingPolicyFixedX)) {
-					for (int i=0; i<(int)patterns.size(); ++i) {
-						ImGui::PushID(i);
-						ImGui::TableNextCell();
-						ImGui::Text("%6d", patterns[i].count);
-
-						ImGui::PushStyleVar(ImGuiStyleVar_CellPadding, ImVec2(0, 0));
-						if (ImGui::BeginTable("patterns", pattern_N)) {
-
-							int j=0;
-							for (int py=0; py<pattern_N; ++py) {
-								for (int px=0; px<pattern_N; ++px) {
-									ImGui::PushID(j++);
-
-									auto id = patterns_data[i * pattern_N*pattern_N + (pattern_N-1 -py) * pattern_N + px];
-									srgba8 col = id != NULLID ? srgba8(tiles[id].color, 255) : srgba8(0,0,0,0);
-
-									ImGui::TableNextCell();
-									colored_button(col, IMGUI_TILE_SZ);
-
-									if (ImGui::IsItemHovered()) {
-										ImGui::BeginTooltip();
-										ImGui::Text(id != NULLID ? tiles[id].name.c_str() : "null");
-										ImGui::EndTooltip();
-									}
-
-									ImGui::PopID();
-								}
-							}
-
-							ImGui::EndTable();
-						}
-						ImGui::PopStyleVar();
-
-						ImGui::PopID();
-					}
-					ImGui::EndTable();
-				}
-
-				ImGui::End();
-			}
 
 			ImGui::End();
+
+			if (show_patterns && ImGui::Begin("Patterns", &show_patterns)) {
+				ImGui::Text("pattern_total: %6d", pattern_total);
+
+				imgui_pattern_list();
+				ImGui::End();
+				
+				if (show_pattern_details && ImGui::Begin(prints("Pattern Details [%d]###Pattern Details", selected_pattern).c_str(), &show_pattern_details)) {
+					
+					for (int d=0; d<4; ++d) {
+						auto& pat = patterns[selected_pattern];
+
+						if (ImGui::TreeNodeEx(
+							prints("Compatible Patterns with offset (%+d,%+d) [%d]###compat[%d]", dirs[d].x,dirs[d].y, pat.compatible_count[d], d).c_str(),
+							ImGuiTreeNodeFlags_DefaultOpen)) {
+
+							imgui_pattern_list(false, &pat._compatible_patterns[d], (int)pat._compatible_patterns[d].size());
+
+							ImGui::TreePop();
+						}
+					}
+					ImGui::End();
+				}
+			}
+		}
+	}
+
+	// draw either all patterns or vector of pattern indexes
+	void imgui_pattern_list (bool show_details=true, std::vector<int>* indexes=nullptr, int count=0) {
+		if (count == 0) count = (int)patterns.size();
+		
+		auto wndsz = ImGui::GetContentRegionAvail();
+
+		float cw = IMGUI_TILE_SZ*N + ImGui::GetStyle().CellPadding.x*2;
+		int w = max(1, min(floori(wndsz.x / cw), count));
+
+		if (ImGui::BeginTable("patterns", w, ImGuiTableFlags_Borders|ImGuiTableFlags_SizingPolicyFixedX)) {
+			for (int i=0; i<count; ++i) {
+				ImGui::PushID(i);
+
+				ImGui::TableNextCell();
+
+				int index = indexes ? (*indexes)[i] : i;
+
+				if (show_details) {
+					if (ImGui::SmallButton("..")) {
+						show_pattern_details = true;
+						selected_pattern = index;
+					}
+					ImGui::SameLine();
+				}
+
+				if (indexes)
+					ImGui::Text("[%d] %4d", index, patterns[index].count);
+				else
+					ImGui::Text("%4d", patterns[index].count);
+
+				ImGui::PushStyleVar(ImGuiStyleVar_CellPadding, ImVec2(0, 0));
+				if (ImGui::BeginTable("patterns", N)) {
+
+					int j=0;
+					for (int py=0; py<N; ++py) {
+						for (int px=0; px<N; ++px) {
+							ImGui::PushID(j++);
+
+							auto id = patterns_data[index * N*N + (N-1 -py) * N + px];
+							srgba8 col = id != NULLID ? srgba8(I.tiles[id].color, 255) : srgba8(0,0,0,0);
+
+							ImGui::TableNextCell();
+							colored_button(col, IMGUI_TILE_SZ);
+
+							if (ImGui::IsItemHovered()) {
+								ImGui::BeginTooltip();
+								ImGui::Text(id != NULLID ? I.tiles[id].name.c_str() : "null");
+								ImGui::EndTooltip();
+							}
+
+							ImGui::PopID();
+						}
+					}
+
+					ImGui::EndTable();
+				}
+				ImGui::PopStyleVar();
+
+				ImGui::PopID();
+			}
+			ImGui::EndTable();
 		}
 	}
 
